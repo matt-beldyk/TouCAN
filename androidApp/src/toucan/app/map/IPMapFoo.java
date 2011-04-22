08aefd7bfe413d1f6a44d052b065a418200f1f15
Matthew Beldyk <matt@beldyk.org>
2011-04-21 22:02:36 -0600

    adding local location foo

--- androidApp/src/toucan/app/map/IPMapFoo.java
+++ androidApp/src/toucan/app/map/IPMapFoo.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
+import com.google.android.maps.MyLocationOverlay;
 
 
 public class IPMapFoo extends MapActivity {
@@ -25,7 +26,7 @@ public class IPMapFoo extends MapActivity {
 	
 	protected void initMap(){
 		this.mapV = (MapView) findViewById(R.id.ip_map);
-//		controller = this.mapV.getController();
+		controller = this.mapV.getController();
 		mapV.setSatellite(true);
 		mapV.setBuiltInZoomControls(true);
 
@@ -33,6 +34,20 @@ public class IPMapFoo extends MapActivity {
 		Drawable monMark = getResources().getDrawable(R.drawable.icon);
 		monMark.setBounds(0,0,monMark.getIntrinsicWidth(),monMark.getIntrinsicHeight());
 	}
+	
+    private void initMyLocation() {
+        final MyLocationOverlay overlay = new MyLocationOverlay(this, mapV);
+        overlay.enableMyLocation(); 
+        //overlay.enableCompass(); // does not work in emulator
+        overlay.runOnFirstFix(new Runnable() {
+           public void run() {
+              // Zoom in to current location
+              controller.setZoom(16);
+              controller.animateTo(overlay.getMyLocation());
+           }
+        });
+        mapV.getOverlays().add(overlay);
+     }
 	/** Called with the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
